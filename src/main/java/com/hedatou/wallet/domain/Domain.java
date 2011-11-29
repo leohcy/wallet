ff42a12fec8694fb82ce548cfe606fee52c5633c
hechaoyi <hechaoyi@me.com>
2011-11-29 14:46:08 +0800

    添加H2数据库支持、Hibernate

---
+++ src/main/java/com/hedatou/wallet/domain/Domain.java
@@ -0,0 +1,52 @@
+package com.hedatou.wallet.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+@MappedSuperclass
+public abstract class Domain {
+
+	private Long id;
+	private Long version;
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Version
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	@Override
+	public int hashCode() {
+		return id == null ? super.hashCode() : id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Domain other = (Domain) obj;
+		if (id != null)
+			return id.equals(other.id);
+		return super.equals(other);
+	}
+
+}
