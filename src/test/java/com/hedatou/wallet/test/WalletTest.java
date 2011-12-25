a2724a79db6f64bb1d2d5fe466be6da631608e39
hechaoyi <hechaoyi@me.com>
2011-12-25 18:06:37 +0800

    1.0版本发布

---
+++ src/test/java/com/hedatou/wallet/test/WalletTest.java
@@ -0,0 +1,15 @@
+package com.hedatou.wallet.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext.xml")
+@TransactionConfiguration(defaultRollback = false)
+@Transactional
+public class WalletTest {
+
+}
